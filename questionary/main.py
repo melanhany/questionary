41cd1de5df8db8f177dc94e8c934df03cd1da950
Yermakhan <melanhany@gmail.com>
2023-12-06 02:46:04 +0300

    added fixture in conftest.py added routing for users endpoint

--- questionary/main.py
+++ questionary/main.py
@@ -1,14 +1,12 @@
 from typing import Union
 from fastapi import FastAPI
+from questionary.api import users
 
 app = FastAPI()
+
+app.include_router(users.router)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,8 @@ from starlette.testclient import TestClient
 
 from questionary.main import app
 
-client = TestClient(app)
 
-
-def test_ping():
-    response = client.get("/")
+def test_ping(test_app):
+    response = test_app.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
